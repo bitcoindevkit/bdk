98d012818f5fdc55dff3cb38575bd62cf2a9b9c1
Tobin Harding <me@tobin.cc>
2020-12-23 14:16:43 +1100

    Use default pattern
    Clippy emits warning:
    
      warning: field assignment outside of initializer for an instance
      created with Default::default()
    
    Do as suggested by clippy and use the default init pattern.
    
    ```
        let foo = Foo {
        	bar: ...,
            Default::default()
        }
    ```

--- src/wallet/mod.rs
+++ src/wallet/mod.rs
@@ -3399,15 +3399,18 @@ mod test {
             .unwrap();
 
         // add another input to the psbt that is at least passable.
-        let mut dud_input = bitcoin::util::psbt::Input::default();
-        dud_input.witness_utxo = Some(TxOut {
-            value: 100_000,
-            script_pubkey: miniscript::Descriptor::<bitcoin::PublicKey>::from_str(
-                "wpkh(025476c2e83188368da1ff3e292e7acafcdb3566bb0ad253f62fc70f07aeee6357)",
-            )
-            .unwrap()
-            .script_pubkey(miniscript::NullCtx),
-        });
+        let dud_input = bitcoin::util::psbt::Input {
+            witness_utxo: Some(TxOut {
+                value: 100_000,
+                script_pubkey: miniscript::Descriptor::<bitcoin::PublicKey>::from_str(
+                    "wpkh(025476c2e83188368da1ff3e292e7acafcdb3566bb0ad253f62fc70f07aeee6357)",
+                )
+                .unwrap()
+                .script_pubkey(miniscript::NullCtx),
+            }),
+            ..Default::default()
+        };
+
         psbt.inputs.push(dud_input);
         psbt.global.unsigned_tx.input.push(bitcoin::TxIn::default());
         let (psbt, is_final) = wallet.sign(psbt, None).unwrap();
