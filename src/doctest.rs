12ae1c3479e11c8889aecd3c7319ea24ca2a1a63
Steve Myers <steve@notmandatory.org>
2021-03-03 13:22:05 -0800

    Update license to Apache 2.0 or MIT, copyright to Bitcoin Dev Kit Developers

--- src/doctest.rs
+++ src/doctest.rs
@@ -1,3 +1,14 @@
+// Bitcoin Dev Kit
+// Written in 2020 by Alekos Filini <alekos.filini@gmail.com>
+//
+// Copyright (c) 2020-2021 Bitcoin Dev Kit Developers
+//
+// This file is licensed under the Apache License, Version 2.0 <LICENSE-APACHE
+// or http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your option.
+// You may not use this file except in accordance with one or both of these
+// licenses.
+
 #[doc(include = "../README.md")]
 #[cfg(doctest)]
 pub struct ReadmeDoctests;
