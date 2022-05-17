93cd896b9c4d929afded3235d0ba84fbd3754fd6
Akshit Talwar <akshittalwar@protonmail.com>
2022-05-17 18:07:23 +0530

    f - rust fmt wordlists/mod.rs

--- src/keys/bip39/wordlists/mod.rs
+++ src/keys/bip39/wordlists/mod.rs
@@ -1,23 +1,23 @@
 // English is always included as the default for BIP39
-mod english;
-#[cfg(feature = "japanese")]
-mod japanese;
-#[cfg(feature = "korean")]
-mod korean;
-#[cfg(feature = "spanish")]
-mod spanish;
 #[cfg(feature = "chinese_simplified")]
 mod chinese_simplified;
 #[cfg(feature = "chinese_traditional")]
 mod chinese_traditional;
+#[cfg(feature = "czech")]
+mod czech;
+mod english;
 #[cfg(feature = "french")]
 mod french;
 #[cfg(feature = "italian")]
 mod italian;
-#[cfg(feature = "czech")]
-mod czech;
+#[cfg(feature = "japanese")]
+mod japanese;
+#[cfg(feature = "korean")]
+mod korean;
 #[cfg(feature = "portuguese")]
 mod portuguese;
+#[cfg(feature = "spanish")]
+mod spanish;
 
 /// List of supported languages for mnemonics
 pub enum Language {
@@ -103,16 +103,46 @@ mod test {
         // The sha256 is calculated from the raw txt file of each language wordlist sourced from
         // [BIP39](https://github.com/bitcoin/bips/blob/master/bip-0039/bip-0039-wordlists.md).
         let checksum = vec![
-            ("2f5eed53a4727b4bf8880d8f3f199efc90e58503646d9ff8eff3a2ed3b24dbda", Language::English),
-            ("2eed0aef492291e061633d7ad8117f1a2b03eb80a29d0e4e3117ac2528d05ffd", Language::Japanese),
-            ("9e95f86c167de88f450f0aaf89e87f6624a57f973c67b516e338e8e8b8897f60", Language::Korean),
-            ("46846a5a0139d1e3cb77293e521c2865f7bcdb82c44e8d0a06a2cd0ecba48c0b", Language::Spanish),
-            ("5c5942792bd8340cb8b27cd592f1015edf56a8c5b26276ee18a482428e7c5726", Language::ChineseSimplified),
-            ("417b26b3d8500a4ae3d59717d7011952db6fc2fb84b807f3f94ac734e89c1b5f", Language::ChineseTraditional),
-            ("ebc3959ab7801a1df6bac4fa7d970652f1df76b683cd2f4003c941c63d517e59", Language::French),
-            ("d392c49fdb700a24cd1fceb237c1f65dcc128f6b34a8aacb58b59384b5c648c2", Language::Italian),
-            ("7e80e161c3e93d9554c2efb78d4e3cebf8fc727e9c52e03b83b94406bdcc95fc", Language::Czech),
-            ("2685e9c194c82ae67e10ba59d9ea5345a23dc093e92276fc5361f6667d79cd3f", Language::Portuguese),
+            (
+                "2f5eed53a4727b4bf8880d8f3f199efc90e58503646d9ff8eff3a2ed3b24dbda",
+                Language::English,
+            ),
+            (
+                "2eed0aef492291e061633d7ad8117f1a2b03eb80a29d0e4e3117ac2528d05ffd",
+                Language::Japanese,
+            ),
+            (
+                "9e95f86c167de88f450f0aaf89e87f6624a57f973c67b516e338e8e8b8897f60",
+                Language::Korean,
+            ),
+            (
+                "46846a5a0139d1e3cb77293e521c2865f7bcdb82c44e8d0a06a2cd0ecba48c0b",
+                Language::Spanish,
+            ),
+            (
+                "5c5942792bd8340cb8b27cd592f1015edf56a8c5b26276ee18a482428e7c5726",
+                Language::ChineseSimplified,
+            ),
+            (
+                "417b26b3d8500a4ae3d59717d7011952db6fc2fb84b807f3f94ac734e89c1b5f",
+                Language::ChineseTraditional,
+            ),
+            (
+                "ebc3959ab7801a1df6bac4fa7d970652f1df76b683cd2f4003c941c63d517e59",
+                Language::French,
+            ),
+            (
+                "d392c49fdb700a24cd1fceb237c1f65dcc128f6b34a8aacb58b59384b5c648c2",
+                Language::Italian,
+            ),
+            (
+                "7e80e161c3e93d9554c2efb78d4e3cebf8fc727e9c52e03b83b94406bdcc95fc",
+                Language::Czech,
+            ),
+            (
+                "2685e9c194c82ae67e10ba59d9ea5345a23dc093e92276fc5361f6667d79cd3f",
+                Language::Portuguese,
+            ),
         ];
 
         for (hash, language) in checksum {
