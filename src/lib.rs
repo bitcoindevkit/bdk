e6f2d029fa9708f98599c1bd4ef74d232b111c5a
Shobit Beltangdy <shobitb@gmail.com>
2022-09-18 12:50:24 -0700

    Remove genesis_block lazy initialization
    This commit contains a change to address issue #752
    
    cargo test runs successfully.

--- src/lib.rs
+++ src/lib.rs
@@ -231,9 +231,6 @@ extern crate async_trait;
 #[macro_use]
 extern crate bdk_macros;
 
-#[cfg(feature = "compact_filters")]
-extern crate lazy_static;
-
 #[cfg(feature = "rpc")]
 pub extern crate bitcoincore_rpc;
 
