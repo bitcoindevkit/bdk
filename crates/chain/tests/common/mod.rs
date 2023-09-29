6e511473a5e5730d5cad237c651c83dc4c5d8756
Daniela Brozzoni <danielabrozzoni@protonmail.com>
2023-09-29 16:02:48 +0200

    test(chain): add block_id! utility macro
    Co-authored-by: Wei Chen <wzc110@gmail.com>

--- crates/chain/tests/common/mod.rs
+++ crates/chain/tests/common/mod.rs
@@ -1,4 +1,14 @@
 #[allow(unused_macros)]
+macro_rules! block_id {
+    ($height:expr, $hash:literal) => {{
+        bdk_chain::BlockId {
+            height: $height,
+            hash: bitcoin::hashes::Hash::hash($hash.as_bytes()),
+        }
+    }};
+}
+
+#[allow(unused_macros)]
 macro_rules! h {
     ($index:literal) => {{
         bitcoin::hashes::Hash::hash($index.as_bytes())
