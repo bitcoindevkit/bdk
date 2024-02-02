9bb39a3a3fcdebeec53a8a771091954c845db7a5
Steven Roose <steven@stevenroose.org>
2024-02-02 02:02:27 +0000

    Avoid a wildcard match in tx construction

--- crates/bdk/src/wallet/mod.rs
+++ crates/bdk/src/wallet/mod.rs
@@ -1347,7 +1347,7 @@ impl<D> Wallet<D> {
             }
             Some(tx_builder::Version(x)) => x,
             None if requirements.csv.is_some() => 2,
-            _ => 1,
+            None => 1,
         };
 
         // We use a match here instead of a unwrap_or_else as it's way more readable :)
