fbc1625a72e49158b34d9d621ac100d59bbb0e6b
Jesus Christ <120573631+Gudnessuche@users.noreply.github.com>
2024-12-05 21:25:36 +0000

    Update compiler.rs
    made comment more explanatory

--- crates/wallet/examples/compiler.rs
+++ crates/wallet/examples/compiler.rs
@@ -26,8 +26,8 @@ use bdk_wallet::{KeychainKind, Wallet};
 /// Miniscript policy is a high level abstraction of spending conditions. Defined in the
 /// rust-miniscript library here  https://docs.rs/miniscript/7.0.0/miniscript/policy/index.html
 /// rust-miniscript provides a `compile()` function that can be used to compile any miniscript policy
-/// into a descriptor. This descriptor then in turn can be used in bdk a fully functioning wallet
-/// can be derived from the policy.
+/// into a descriptor. This descriptor then in turn can be used in bdk as a fully functioning wallet
+/// that can be derived from the policy.
 ///
 /// This example demonstrates the interaction between a bdk wallet and miniscript policy.
 
