568a36642c43d74132339a4e42dd41c545a764e1
valued mammal <valuedmammal@protonmail.com>
2025-06-07 10:43:11 -0400

    refactor(esplora): in debug build assert that `latest_blocks` is not empty

--- crates/esplora/src/blocking_ext.rs
+++ crates/esplora/src/blocking_ext.rs
@@ -190,9 +190,15 @@ fn fetch_block(
 
     // We avoid fetching blocks higher than previously fetched `latest_blocks` as the local chain
     // tip is used to signal for the last-synced-up-to-height.
-    if let Some(tip_height) = latest_blocks.keys().last().copied() {
-        if height > tip_height {
+    match latest_blocks.keys().last().copied() {
+        None => {
+            debug_assert!(false, "`latest_blocks` should not be empty");
             return Ok(None);
+        }
+        Some(tip_height) => {
+            if height > tip_height {
+                return Ok(None);
+            }
         }
     }
 
